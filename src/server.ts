9cb1016bc9b6f3eaab7fc601a4b043b5888b71c4
lmorais <leozonnn@gmail.com>
2021-11-25 16:51:57 -0300

    feat: implementation mongo connect and error handling

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,36 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import mongoose from 'mongoose';
+import AppError from './errors/AppError'
 
 const app = express();
 
 app.use(express.json());
+
+mongoose.connect('mongodb://localhost:15017/graphql_code', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use(express.json());
+
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        message: error.message,
+        status: 'Error',
+      });
+    }
+
+    console.log('Error', error.message);
+
+    return response.status(500).json({
+      message: 'Internal Server Error',
+      status: 'Error',
+    });
+  }
+);
 
 app.listen(5000, () => {
   console.log('Server is running')
